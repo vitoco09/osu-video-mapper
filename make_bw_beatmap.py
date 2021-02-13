e41dfe33068b0326f6e7a2ecc82e6fe9ccb5f92e
Victor Benjamin Vidal Paz <vvidal.paz@gmail.com>
2021-02-13 00:38:13 -0300

    Fixed some issues

--- make_bw_beatmap.py
+++ make_bw_beatmap.py
@@ -2,9 +2,13 @@ import sys
 import os
 from PIL import Image
 
+# WORKDIR = 'D:/projects/osu-video-mapper/workdir'
+# BEATMAP_OUTPUT = 'D:/projects/osu-video-mapper/valecallampalacueca.osu'
+# FPS = 20
+
 WORKDIR = sys.argv[1]
 BEATMAP_OUTPUT = sys.argv[2]
-FPS = sys.argv[3]
+FPS = int(sys.argv[3])
 NUM_FRAMES = len([name for name in os.listdir(WORKDIR) if os.path.isfile(os.path.join(WORKDIR, name))])
 
 # Value in ms.
@@ -16,14 +20,14 @@ for frame in range(NUM_FRAMES):
 
     current_frame = Image.open(WORKDIR + f'/image_{frame+1}.png')
     # We transform it to black and white, not the same as greyscale.
-    current_frame_bw = current_frame.convert('1')  # Either 0 for black or 255 for white
+    current_frame_bw = current_frame.convert('L')  # Either 0 for black or 255 for white
 
     for i in range(32):     # CS10 max width:  32
         for j in range(24): # CS10 max height: 24
 
             bw = current_frame_bw.getpixel((i,j))
 
-            if bw == 255:  # White
+            if bw < 128:  # White-ish
                 hitcircle = f'{16*(i+1)},{16*(j+1)},{round(1337 + FRAME_PERIOD*frame)},1,0,0:0:0:0:'
                 hitcircles.append(hitcircle)
 
